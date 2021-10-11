283593a7e58d77511a1fa1da566cc8f0b1dc5c55
Dannie Vinther <davi@kea.dk>
2021-10-11 09:18:44 +0200

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+  const h2 = section.querySelector("h2");
+  const p = section.querySelectorAll("p");
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: "50% 50%",
+        end: "+=200",
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    })
+    .from(h2, {
+      duration: 1,
+      opacity: 0,
+    })
+    .from(p, {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 1,
+    });
+});
